a4cba7bed58b723d4aec6bf269eb6efff39c9096
shreyaballijepalli <cs15btech11009@iith.ac.in>
2019-04-07 15:23:16 +0530

    Part 1

---
+++ application.py
@@ -0,0 +1,127 @@
+import os, jinja2, json
+from flask import Flask, flash, render_template, request, redirect, url_for, send_from_directory, Blueprint, session
+from werkzeug import secure_filename
+from flask_oauth import OAuth
+
+from app import app, cursor, db
+
+application = Blueprint('application', __name__, template_folder='templates', static_folder='static')   
+
+
+@application.route('/part1', methods=['GET'])       #on submission of login details
+def part1(): 
+	#insert
+	return render_template('application_part1.html')
+	return render_template('application_placeholders_part1.html',params=params)
+
+
+@application.route('/part2', methods=['GET'])       #on submission of login details
+def part2(): 
+   return render_template('application_part2.html')
+
+
+
+
+@application.route('/insert_1', methods=['GET','POST'])       #on submission of login details
+def insert_1(): 
+	if (request.method =='POST'):
+		position = request.form['position']
+		firstname = request.form['first_name']
+		middlename = request.form['middle_name']
+		lastname = request.form['last_name']
+		name = "("+firstname+","+middlename+","+lastname+")"
+		address1 = request.form['address_1']
+		address2 = request.form['address_2']
+		address3 = request.form['address_3']
+		address = address1+address2+address3
+		email = request.form['email']
+		altemail = request.form['alt_email']
+		nationality = request.form['nationality']
+		age = request.form['age']
+		date_of_birth = request.form['date_of_birth']
+		caste = request.form['caste']
+		disability = request.form['disability']
+		other_info = request.form['other_info']
+		# photo = request.form['photo']
+		# signature = request.form['signature']
+
+		params = [position,firstname,middlename,lastname,nationality,age,date_of_birth,
+		caste,email,altemail,disability,address1,address2,address3,other_info]
+
+#update
+		sql = "INSERT INTO main_table (position_applied, name, address, email, alt_email,\
+		 nationality,age,date_of_birth, caste, disability,other_info) \
+		VALUES('%s','%s','%s','%s','%s','%s','%d','%s','%s','%s','%s') " \
+		% (position,name,address,email,altemail,nationality,
+		  int(age),date_of_birth,caste,disability,other_info)
+
+		try:   
+		   cursor.execute(sql)
+		   db.commit()
+		   print "Form personal info is stored"
+		except:
+			print "Info error"
+
+	#return "Saved!"
+   	return redirect(url_for("/part1"))
+
+
+
+@application.route('/insert_2', methods=['GET','POST'])       #on submission of login details
+def insert_2(): 
+	phd_date_studied = request.form['phd_university_date']
+	phd_university = request.form['phd_university']
+	phd_specialization = request.form['phd_specialization']
+	phd_date_thesis = request.form['phd_date_thesis']
+	phd_date_defence = request.form['phd_date_defence']
+	phd_edu_info = "("+phd_date_studied+","+phd_university+","+phd_specialization+")"
+	phd_info = "("+phd_edu_info+","+phd_date_thesis+","+phd_date_defence+")"
+
+
+	masters_date_studied = request.form['masters_university_date']
+	masters_university = request.form['masters_university']
+	masters_specialization = request.form['masters_specialization']
+	masters_info = "("+masters_date_studied+","+masters_university+","+masters_specialization+")"
+
+	bachelors_date_studied = request.form['bachelors_university_date']
+	bachelors_university = request.form['bachelors_university']
+	bachelors_specialization = request.form['bachelors_specialization']
+	bachelors_info = "("+bachelors_date_studied+","+bachelors_university+","+bachelors_specialization+")"
+
+	gate_type = request.form['gate_type']
+	gate_score = request.form['gate_score']
+	gate_info = "("+gate_type+","+gate_score+")"
+
+	research_specialization = request.form['research_specialization']
+	research_interest = request.form['research_interest']
+	research_info = "("+research_specialization+","+research_interest+")"
+
+	print "check research ",research_interest
+
+	post_doc = request.form['post_doc_spec']
+
+	print "check  ",post_doc
+
+	# post_doc1 = request.form['post_doc1']
+	# post_doc2 = request.form['post_doc2']
+	# post_doc3 = request.form['post_doc3']
+
+	# post_doc = "{"+post_doc1+","+post_doc2+","+post_doc3+"}"
+
+	params = []
+
+
+	sql = "INSERT INTO education (phd,phd_thesis, masters, bachelors, gate,research,\
+	 post_doc) \
+	VALUES('%s','%s',%s','%s','%s','%s','%s') " \
+	% (phd_edu_info,phd_info,masters_info,bachelors_info,gate_info,research_info,post_doc)
+
+	try:   
+	   cursor.execute(sql)
+	   db.commit()
+	   print "Form education is stored"
+	except:
+		print "Info error"
+
+	return "Saved!"
+   	# return render_template('application_placeholders_part1.html',params=params)
---
+++ authentication.py
@@ -0,0 +1,116 @@
+import os, jinja2, json
+from flask import Flask, flash, render_template, request, redirect, url_for, send_from_directory, Blueprint, session
+from werkzeug import secure_filename
+from flask_oauth import OAuth
+from flask_login import logout_user, confirm_login, LoginManager
+
+GOOGLE_CLIENT_ID = '968468250852-0hgdrduaga14on3nqo72mhhi0aovspel.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET = 'AowoVK79d8yLqWt7M-5cy3mJ'
+REDIRECT_URI = '/oauth2callback'  # one of the Redirect URIs from Google APIs console
+ 
+SECRET_KEY = 'development key'
+DEBUG = True
+
+from app import app, cursor, db
+app = Flask(__name__)
+app.debug = DEBUG
+app.secret_key = SECRET_KEY
+oauth = OAuth()
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+from application import application
+app.register_blueprint(application, url_prefix='/application')
+ 
+google = oauth.remote_app('google',
+base_url='https://www.google.com/accounts/',
+authorize_url='https://accounts.google.com/o/oauth2/auth',
+request_token_url=None,
+request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',
+'response_type': 'code'},
+access_token_url='https://accounts.google.com/o/oauth2/token',
+access_token_method='POST',
+access_token_params={'grant_type': 'authorization_code'},
+consumer_key=GOOGLE_CLIENT_ID,
+consumer_secret=GOOGLE_CLIENT_SECRET)
+
+results = []
+name_ = ""
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/verify/')
+def verify():
+	access_token = session.get('access_token')
+	if access_token is None:
+		return redirect(url_for('login'))
+ 
+	access_token = access_token[0]
+	from urllib2 import Request, urlopen, URLError
+	 
+	headers = {'Authorization': 'OAuth '+access_token}
+	req = Request('https://www.googleapis.com/oauth2/v1/userinfo',
+	None, headers)
+	try:
+		res = urlopen(req)
+	except URLError, e:
+		if e.code == 401:
+			# Unauthorized - bad token
+			session.pop('access_token', None)
+			return redirect(url_for('login'))
+		return res.read()
+	 
+	result = res.read()
+	print 'res read ',result
+	d = json.loads(result)
+	global name_ 
+	name_ = d['email']
+	print d['email']
+	
+	sql = "SELECT * FROM main_table WHERE email = '%s'" % (d['email'])       #checking if user is already there in database
+	cursor.execute(sql)
+	global results
+	results = cursor.fetchall()
+	if results == []:
+		query = "INSERT INTO main_table(email) VALUES ('"+d['email']+"')"
+		cursor.execute(query)
+		db.commit()
+
+	confirm_login()
+	# set_params(results)
+	return redirect(url_for('show_applications')) 
+	# else:
+	# 	print "existing"
+	# 	return redirect(url_for('menu.show_menu'))
+
+@app.route('/menu/', methods=['GET'])       #on submission of login details
+def show_applications(): 
+	return render_template('show_application.html', rows=results, name_=name_)
+
+@app.route('/login')
+def login():
+	callback=url_for('authorized', _external=True)
+	return google.authorize(callback=callback)
+ 
+@app.route("/logout/")
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route(REDIRECT_URI)
+@google.authorized_handler
+def authorized(resp):
+	access_token = resp['access_token']
+	session['access_token'] = access_token, ''
+	return redirect(url_for('verify'))
+ 
+@google.tokengetter
+def get_access_token():
+	return session.get('access_token')
+
+ 
+if __name__ == '__main__':
+	app.run(debug = True)
